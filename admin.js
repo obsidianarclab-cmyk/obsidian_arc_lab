ac2909d694ca633cfac335c466c94cf985a081b3
obsidianarclab-cmyk <obsidianarclab@gmail.com>
2026-08-16 15:32:59 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
 if(sessionStorage.getItem('oa_admin')!=='yes') location.replace('admin-login.html');
 
 const KEY='obsidian_arc_records_v1';
-const SHEETS_WEB_APP_URL='https://script.google.com/macros/s/AKfycbyDMpV49Tfb2Fos1g3wEaG9_ig4KBz_gYfmyAyjCtqpX5MwUr8FKrvGwrqPI6a2_LSZcw/exec';
+const SHEETS_WEB_APP_URL='https://script.google.com/macros/s/AKfycby7k-QT6oWy7zOClDqKXVqYRmDhGyhTKhokqb4Vdt7zs-YiZfC_xQV-O9GXW-7v4K3Qdw/exec';
 const SHEETS_API_KEY='obsidian_arc_lab';
 const sheetsEnabled=()=>SHEETS_WEB_APP_URL.startsWith('https://script.google.com/macros/s/')&&SHEETS_WEB_APP_URL.endsWith('/exec');
 let data=JSON.parse(localStorage.getItem(KEY)||'{"orders":[],"expenses":[],"products":[],"inventory":[]}');
